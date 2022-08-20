5b27b3c2e025b44dd8d2304134c77b1e9af65b6d
Adriel <Adriel@192.168.5.101>
2022-08-20 19:54:27 -0300

    teste da aula de git

--- jdev-treinamento-aulas-java/src/com/jdev/aulas/tiposdedados/TipoInteiro.java
+++ jdev-treinamento-aulas-java/src/com/jdev/aulas/tiposdedados/TipoInteiro.java
@@ -5,8 +5,10 @@ public class TipoInteiro {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// Para teste do git
 		int numeros;
 		int quantidades;
+		
 	
 	}
 
