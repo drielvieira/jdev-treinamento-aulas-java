e9045034e60f6be59033f163a099cd8978100572
Adriel <Adriel@192.168.5.101>
2022-08-20 20:47:41 -0300

    Criando a classe variavel e adicionando os tipos de dados

---
+++ jdev-treinamento-aulas-java/src/com/jdev/aulas/tiposdedados/Variaveis.java
@@ -0,0 +1,16 @@
+package com.jdev.aulas.tiposdedados;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		// a variable references the data type
+		byte tipoByte;
+		short tipoShort;
+		int tipoInt;
+		boolean tipoBoolean;
+		String tipoText;
+		// BigDecimal tipoBigDecimal;
+
+	}
+
+}
