3990fee6fa7724119c623960c06f5a56d95db0f8
Adriel <Adriel@192.168.5.101>
2022-08-20 19:49:08 -0300

    adicionando classes e pacotes

---
+++ jdev-treinamento-aulas-java/src/com/jdev/aulas/operadoreslogicos/If.java
@@ -0,0 +1,10 @@
+package com.jdev.aulas.operadoreslogicos;
+
+public class If {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ jdev-treinamento-aulas-java/src/com/jdev/aulas/tiposdedados/TipoInteiro.java
@@ -0,0 +1,10 @@
+package com.jdev.aulas.tiposdedados;
+
+public class TipoInteiro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
