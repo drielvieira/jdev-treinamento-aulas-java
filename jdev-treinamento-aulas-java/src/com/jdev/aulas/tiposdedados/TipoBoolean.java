03b91fce5ebdf387dfd414b1e1f2f9ccd9db831b
Adriel <Adriel@192.168.5.101>
2022-08-20 21:40:41 -0300

    Criando classes para entender os tipos

---
+++ jdev-treinamento-aulas-java/src/com/jdev/aulas/tiposdedados/TipoBoolean.java
@@ -0,0 +1,22 @@
+package com.jdev.aulas.tiposdedados;
+
+public class TipoBoolean {
+
+	public static void main(String[] args) {
+		
+		boolean logicYes = true;
+		boolean logicNot = false;
+		
+		System.out.println("Gosta de futebol? : ");
+		System.out.println(logicNot + " ,não gosto de futebol");
+		// aproveito e concateno
+		
+		System.out.println("=======================================");
+		
+		System.out.println("Gosta de futebol? : ");
+		System.out.println(logicYes + ", sim gosto de futebol");
+		// concateno
+
+	}
+
+}
