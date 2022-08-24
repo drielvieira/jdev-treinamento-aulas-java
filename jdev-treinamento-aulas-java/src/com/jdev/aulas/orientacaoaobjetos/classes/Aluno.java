9f0dcdaf3f151473196485b45cabdb9968b71163
Adriel <Adriel@192.168.5.101>
2022-08-23 21:55:27 -0300

    adicionando atributo a classe aluno

--- jdev-treinamento-aulas-java/src/com/jdev/aulas/orientacaoaobjetos/classes/Aluno.java
+++ jdev-treinamento-aulas-java/src/com/jdev/aulas/orientacaoaobjetos/classes/Aluno.java
@@ -2,5 +2,15 @@ package com.jdev.aulas.orientacaoaobjetos.classes;
 
 /* Classe Objeto que representa aluno*/
 public class Aluno {
+	String nome;
+	int idade;
+	String dataNascimento;
+	String registroGeral;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculado;
 
 }
