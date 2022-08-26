3995545056c7e283259cac94d0c4035cd663a42a
Adriel <Adriel@192.168.5.101>
2022-08-26 00:08:41 -0300

    Metodo principal para usar o construtor

---
+++ jdev-treinamento-aulas-java/src/com/jdev/aulas/orientacaoaobjetos/classes/MainAlunoConstrutorEx.java
@@ -0,0 +1,21 @@
+package com.jdev.aulas.orientacaoaobjetos.classes;
+
+
+public class MainAlunoConstrutorEx {
+
+	public static void main(String[] args) {
+		/* Criando um construtor para o objeto
+		 * ou seja criar o objeto em memoria */
+		
+		/* AGORA TEMOS UM OBJETO REAL NA MEMORIA */
+		AlunoConstrutorEx aluno1 = new AlunoConstrutorEx();
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
