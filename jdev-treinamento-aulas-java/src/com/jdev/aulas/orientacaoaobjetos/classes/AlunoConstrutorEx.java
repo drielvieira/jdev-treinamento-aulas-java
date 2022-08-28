6d039f8ce0431b38dda1a26c8f11f70864772d03
Adriel <Adriel@192.168.5.101>
2022-08-27 22:44:37 -0300

    Adicionando métodos set e get.

--- jdev-treinamento-aulas-java/src/com/jdev/aulas/orientacaoaobjetos/classes/AlunoConstrutorEx.java
+++ jdev-treinamento-aulas-java/src/com/jdev/aulas/orientacaoaobjetos/classes/AlunoConstrutorEx.java
@@ -1,18 +1,19 @@
 package com.jdev.aulas.orientacaoaobjetos.classes;
 
 public class AlunoConstrutorEx {
-		String nome;
-		int idade;
-		String dataNascimento;
-		String registroGeral;
-		String numeroCpf;
-		String nomeMae;
-		String nomePai;
-		String dataMatricula;
-		String nomeEscola;
-		String serieMatriculado;
+		/*ATRIBUTOS*/
+		private String nome;
+		private int idade;
+		private	String dataNascimento;
+		private	String registroGeral;
+		private	String numeroCpf;
+		private	String nomeMae;
+		private	String nomePai;
+		private	String dataMatricula;
+		private	String nomeEscola;
+		private	String serieMatriculado;
 
-		/* UM CONSTRUTOR VAZIO É UMA BOA PRATICA DA LLINGUAGEM 
+		/* UM CONSTRUTOR VAZIO É UMA BOA PRATICA DA LINGUAGEM 
 		 * APESAR DE QUE O JAVA CRIA ESSE CONSTRUTOR INTERNAMETE CASO O DEV NÃO FAZ 
 		 * É PADRÃO */
 		public AlunoConstrutorEx() {
@@ -21,12 +22,98 @@ public class AlunoConstrutorEx {
 		
 		/*PODEMOS CRIAR OUTROS CONSTRUTORES PARA USAR EX: */
 		
-		public AlunoConstrutorEx(String nomePadrao) {
+	/*	public AlunoConstrutorEx(String nomePadrao) {
 			nome = nomePadrao;
 		}
 		
-		public AlunoConstrutorEx(String nomePadrao, int idadePadrao) {
+		public AlunoConstrutorEx(String nomePadrao, String dataNascimentoPadrao ,int idadePadrao) {
 			nome = nomePadrao;
 			idade = idadePadrao;
+			dataNascimento = dataNascimentoPadrao;
+		} */
+		
+		/*METODOS SETTERS E GETTERS DO OBJETO */
+		
+		/*MÉTODO SET QUE RECEBE DADOS*/
+		public void setNome(String nome) {
+			this.nome = nome;
 		}
+		
+		public String getNome() {
+			return nome;
+		}
+		
+		public void setIdade(int idade) {
+			this.idade = idade;
+		}
+		
+		public int getIdade() {
+			return idade;
+		}
+		
+		public void setDataNascimento(String dataNascimento) {
+			this.dataNascimento = dataNascimento;
+		} 
+		
+		public String getDataNascimento() {
+			return dataNascimento;
+		}
+
+		public String getRegistroGeral() {
+			return registroGeral;
+		}
+
+		public void setRegistroGeral(String registroGeral) {
+			this.registroGeral = registroGeral;
+		}
+
+		public String getNumeroCpf() {
+			return numeroCpf;
+		}
+
+		public void setNumeroCpf(String numeroCpf) {
+			this.numeroCpf = numeroCpf;
+		}
+
+		public String getNomeMae() {
+			return nomeMae;
+		}
+
+		public void setNomeMae(String nomeMae) {
+			this.nomeMae = nomeMae;
+		}
+
+		public String getNomePai() {
+			return nomePai;
+		}
+
+		public void setNomePai(String nomePai) {
+			this.nomePai = nomePai;
+		}
+
+		public String getDataMatricula() {
+			return dataMatricula;
+		}
+
+		public void setDataMatricula(String dataMatricula) {
+			this.dataMatricula = dataMatricula;
+		}
+
+		public String getNomeEscola() {
+			return nomeEscola;
+		}
+
+		public void setNomeEscola(String nomeEscola) {
+			this.nomeEscola = nomeEscola;
+		}
+
+		public String getSerieMatriculado() {
+			return serieMatriculado;
+		}
+
+		public void setSerieMatriculado(String serieMatriculado) {
+			this.serieMatriculado = serieMatriculado;
+		}
+		
+		
 }
--- jdev-treinamento-aulas-java/src/com/jdev/aulas/orientacaoaobjetos/classes/MainAlunoConstrutorEx.java
+++ jdev-treinamento-aulas-java/src/com/jdev/aulas/orientacaoaobjetos/classes/MainAlunoConstrutorEx.java
@@ -4,12 +4,54 @@ package com.jdev.aulas.orientacaoaobjetos.classes;
 public class MainAlunoConstrutorEx {
 
 	public static void main(String[] args) {
-		/* Criando um construtor para o objeto
-		 * ou seja criar o objeto em memoria */
+		/* CRIANDO UM CONSTRUTOR PARA O OBJETO
+		 * OU SEJA CRIANDO UM OBJETO EM MEMORIA */
 		
 		/* AGORA TEMOS UM OBJETO REAL NA MEMORIA */
-		AlunoConstrutorEx aluno1 = new AlunoConstrutorEx();
+							/*REFERENCIA*/		/*INSTANCIA*/
+		AlunoConstrutorEx 	aluno1 =		 new AlunoConstrutorEx();
 		
+		
+	
+		
+		aluno1.setNome("ANTONIO PERERIA JUBARTI");
+		aluno1.setIdade(78);
+		aluno1.setDataNascimento("14/05/1888");
+		aluno1.setNomeMae("JOANA APARECIDA");
+		
+		System.out.println("NOME: " + aluno1.getNome());
+		System.out.println("IDADE:" + aluno1.getIdade() + " ANOS");
+		System.out.println("DATA DE NASCIMENTO: " + aluno1.getDataNascimento());
+		System.out.println("NOME DA MÃE: " + aluno1.getNomeMae());
+		
+		System.out.println("===================================================");
+		
+		AlunoConstrutorEx   aluno2 = 		new AlunoConstrutorEx();
+		
+		aluno2.setNome("JOSE MATIAS ANGELO");
+		aluno2.setIdade(55);
+		aluno2.setDataNascimento("18/09/2055");
+		aluno2.setNomeMae("ANTONIA SILVEIRA");
+		
+		System.out.println("NOME: " + aluno2.getNome());
+		System.out.println("IDADE:" + aluno2.getIdade() + " ANOS");
+		System.out.println("DATA DE NASCIMENTO: " + aluno2.getDataNascimento());
+		System.out.println("NOME DA MÃE: " + aluno2.getNomeMae());
+		
+		System.out.println("===================================================");
+		
+		
+		AlunoConstrutorEx   aluno3 = 		new AlunoConstrutorEx();
+		
+		aluno3.setNome("ANTONIA PERERIA ");
+		aluno3.setIdade(66);
+		aluno3.setDataNascimento("14/05/1858");
+		aluno3.setNomeMae("JOAQUINA ATONIETA SAMPAIO");
+		
+		System.out.println("NOME: " + aluno3.getNome());
+		System.out.println("IDADE:" + aluno3.getIdade() + " ANOS");
+		System.out.println("DATA DE NASCIMENTO: " + aluno3.getDataNascimento());
+		System.out.println("NOME DA MÃE: " + aluno3.getNomeMae());
 		
 		
 		
