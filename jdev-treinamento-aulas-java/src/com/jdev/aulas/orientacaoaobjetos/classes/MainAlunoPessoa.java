3cd19ddacee84f621e8586ff379f9dc74b73b3ce
Adriel <Adriel@192.168.5.101>
2022-08-31 23:46:53 -0300

    executavel do objeto AlunoPessoa

---
+++ jdev-treinamento-aulas-java/src/com/jdev/aulas/orientacaoaobjetos/classes/MainAlunoPessoa.java
@@ -0,0 +1,37 @@
+package com.jdev.aulas.orientacaoaobjetos.classes;
+
+import javax.swing.JOptionPane;
+
+public class MainAlunoPessoa {
+
+	public static void main(String[] args) {
+		
+		String nome = JOptionPane.showInputDialog("QUAL É O NOME DO ALUNO? ");
+		String idade = JOptionPane.showInputDialog("QUAL É A IDADE DO ALUNO? ");
+		String dataNascimento = JOptionPane.showInputDialog("QUAL É A DATA DE NASCIMENTO ? ");
+		
+		String nota1 = JOptionPane.showInputDialog("PRIMEIRA NOTA: ");
+		String nota2 = JOptionPane.showInputDialog("SEGUNDA NOTA: ");
+		String nota3 = JOptionPane.showInputDialog("TERCEIRA NOTA: ");
+		String nota4 = JOptionPane.showInputDialog("QUARTA NOTA: ");
+		
+		AlunoPessoa alunoPessoa1 = new AlunoPessoa();
+		
+		alunoPessoa1.setNome(nome);
+		alunoPessoa1.setIdade(Integer.valueOf(idade));
+		alunoPessoa1.setDataNascimento(dataNascimento);
+		
+		alunoPessoa1.setNota1(Double.parseDouble(nota1));
+		alunoPessoa1.setNota2(Double.parseDouble(nota2));
+		alunoPessoa1.setNota3(Double.parseDouble(nota3));
+		alunoPessoa1.setNota4(Double.parseDouble(nota4));
+		
+		System.out.println("NOME DO ALUNO:" + alunoPessoa1.getNome());
+		System.out.println("IDADE DO ALUNO: "+ alunoPessoa1.getIdade() + " ANOS");
+		System.out.println("DATA DE NASCIMENTO: " + alunoPessoa1.getDataNascimento());
+		System.out.println("MÉDIA: " + alunoPessoa1.getMediaNota());
+		System.out.println("RESULTADO: " + (alunoPessoa1.getAlunoAprovado()? "APROVADO" : "REPROVADO"));
+
+	}
+
+}
