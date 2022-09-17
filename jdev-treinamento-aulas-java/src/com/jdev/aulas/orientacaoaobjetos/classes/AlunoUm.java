fec420f44e029f786b9351031a24204e214f7679
Adriel <Adriel@192.168.5.101>
2022-09-16 22:49:38 -0300

    Aula sobre lista

--- jdev-treinamento-aulas-java/src/com/jdev/aulas/orientacaoaobjetos/classes/AlunoUm.java
+++ jdev-treinamento-aulas-java/src/com/jdev/aulas/orientacaoaobjetos/classes/AlunoUm.java
@@ -1,129 +1,144 @@
 package com.jdev.aulas.orientacaoaobjetos.classes;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class AlunoUm {
 
-	/*ATRIBUTOS*/
+	/* ATRIBUTOS */
 	private String nome;
 	private int idade;
-	private	String dataNascimento;
-	private	String registroGeral;
-	private	String numeroCpf;
-	private	String nomeMae;
-	private	String nomePai;
-	private	String dataMatricula;
-	private	String nomeEscola;
-	private	String serieMatriculado;
+	private String dataNascimento;
+	private String registroGeral;
+	private String numeroCpf;
+	private String nomeMae;
+	private String nomePai;
+	private String dataMatricula;
+	private String nomeEscola;
+	private String serieMatriculado;
+
+	private List<DisciplinaAlunoUm> disciplinas = new ArrayList<DisciplinaAlunoUm>();
 	
-	private DisciplinaAlunoUm disciplina = new DisciplinaAlunoUm();
-	
-	public void setDisciplina(DisciplinaAlunoUm disciplina) {
-		this.disciplina = disciplina;
+	public void setDisciplinas(List<DisciplinaAlunoUm> disciplinas) {
+		this.disciplinas = disciplinas;
 	}
 	
-	public DisciplinaAlunoUm getDisciplina() {
-		return disciplina;
+	public List<DisciplinaAlunoUm> getDisciplinas() {
+		return disciplinas;
 	}
-	
-	public AlunoUm() {}
-	
-	
-	
+
+	public AlunoUm() {
+	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public int getIdade() {
 		return idade;
 	}
+
 	public void setIdade(int idade) {
 		this.idade = idade;
 	}
+
 	public String getDataNascimento() {
 		return dataNascimento;
 	}
+
 	public void setDataNascimento(String dataNascimento) {
 		this.dataNascimento = dataNascimento;
 	}
+
 	public String getRegistroGeral() {
 		return registroGeral;
 	}
+
 	public void setRegistroGeral(String registroGeral) {
 		this.registroGeral = registroGeral;
 	}
+
 	public String getNumeroCpf() {
 		return numeroCpf;
 	}
+
 	public void setNumeroCpf(String numeroCpf) {
 		this.numeroCpf = numeroCpf;
 	}
+
 	public String getNomeMae() {
 		return nomeMae;
 	}
+
 	public void setNomeMae(String nomeMae) {
 		this.nomeMae = nomeMae;
 	}
+
 	public String getNomePai() {
 		return nomePai;
 	}
+
 	public void setNomePai(String nomePai) {
 		this.nomePai = nomePai;
 	}
+
 	public String getDataMatricula() {
 		return dataMatricula;
 	}
+
 	public void setDataMatricula(String dataMatricula) {
 		this.dataMatricula = dataMatricula;
 	}
+
 	public String getNomeEscola() {
 		return nomeEscola;
 	}
+
 	public void setNomeEscola(String nomeEscola) {
 		this.nomeEscola = nomeEscola;
 	}
+
 	public String getSerieMatriculado() {
 		return serieMatriculado;
 	}
+
 	public void setSerieMatriculado(String serieMatriculado) {
 		this.serieMatriculado = serieMatriculado;
 	}
-	
-	
+
 	public double getMediaNota() {
-		return (disciplina.getNota1() + disciplina.getNota2()
-		+ disciplina.getNota3()
-		+ disciplina.getNota4()) / 4;
+		return 0;
 	}
-	
+
 	public boolean getAlunoAprovado() {
 		double media = this.getMediaNota();
 		if (media >= 70) {
 			return true;
-		}else {
+		} else {
 			return false;
 		}
 	}
-	
-	
-	
-	
+
+
+
 	@Override
 	public String toString() {
 		return "AlunoUm [nome=" + nome + ", idade=" + idade + ", dataNascimento=" + dataNascimento + ", registroGeral="
 				+ registroGeral + ", numeroCpf=" + numeroCpf + ", nomeMae=" + nomeMae + ", nomePai=" + nomePai
 				+ ", dataMatricula=" + dataMatricula + ", nomeEscola=" + nomeEscola + ", serieMatriculado="
-				+ serieMatriculado + ", disciplina=" + disciplina + "]";
+				+ serieMatriculado + "]";
 	}
-	
-	
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(idade, nome);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -135,11 +150,5 @@ public class AlunoUm {
 		AlunoUm other = (AlunoUm) obj;
 		return idade == other.idade && Objects.equals(nome, other.nome);
 	}
-	
-	
-	
-	
-	
-	
-	
+
 }
